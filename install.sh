c221fd01edf4beaa1c89063b21f8be89b87b2bbf
amb1s1 <amb1s1@gmail.com>
2021-09-22 17:49:32 +0000

    Added new script files to install and configure for the eve-ng server.
    Added logic to run the script files.

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Avoid grub config gui prompt
+sed -i "s/#\ conf_force_conffold=YES/conf_force_conffold=YES/g" /etc/ucf.conf
+
+
+wget -O - http://www.eve-ng.net/repo/install-eve.sh | bash -i
+sudo apt-get update
+sudo apt-get -y upgrade
+# Avoding OS to rename eth0 interface to ens4
+sed -i 's/GRUB_CMDLINE_LINUX.*$/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
+sed -i "s/conf_force_conffold=YES/#conf_force_conffold=YES/g" /etc/ucf.conf
