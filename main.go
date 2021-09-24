284ad0255ec6586baf3f72e60c4ae360edc400ae
amb1s1 <amb1s1@gmail.com>
2021-09-24 14:07:08 +0000

    Change the way we config goeve from flags to yaml config file.

--- main.go
+++ main.go
@@ -5,22 +5,9 @@
 package main
 
 import (
-	"flag"
 	"go-eve/goeve"
 )
 
-var (
-	projectID              = flag.String("project", "", "name of your project")
-	instanceName           = flag.String("instance_name", "", "name of your compute instance")
-	zone                   = flag.String("zone", "us-central1-a", "default to us-central1-a zone")
-	sshPublicKeyFileName   = flag.String("ssh_public_key_file_name", "", "path to the file containing your ssh public key.")
-	sshPrivateKeyFileName  = flag.String("ssh_private_key_file_name", "", "path to the file containing your ssh private key.")
-	sshKeyUsername         = flag.String("ssh_key_username", "", "use the username from your ssh public key. If appear on your ssh public key file. If not do not use this flag. E.g user@domain.")
-	createCustomEveNGImage = flag.Bool("create_custom_eve_ng_image", false, "Create a custom eve-ng image if not already created")
-	customEveNGImageName   = flag.String("custom_eve_ng_image_name", "eve-ng", "Create a custom eve-ng image if not already created. Default is eve-ng")
-)
-
 func main() {
-	flag.Parse()
-	goeve.Run(*projectID, *instanceName, *zone, *sshPublicKeyFileName, *sshPrivateKeyFileName, *sshKeyUsername, *customEveNGImageName, *createCustomEveNGImage)
+	goeve.Run()
 }
