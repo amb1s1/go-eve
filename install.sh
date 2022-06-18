8cfc2336ea6f940ffae85753ffac38e0ca337e26
amb1s1 <amb1s1@gmail.com>
2022-06-18 15:55:51 -0400

    Update install.sh
    Dialog was not install on my previews test. I need to take a look what version of ubuntu the script is installing and why this dialog was install before and not now.

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ sed -i "s/#\ conf_force_conffold=YES/conf_force_conffold=YES/g" /etc/ucf.conf
 wget -O - http://www.eve-ng.net/repo/install-eve.sh | bash -i
 sudo apt-get update
 sudo apt-get -y upgrade
+sudo apt-get install dialog
 # Avoiding OS to rename eth0 interface to ens4
 sudo sed -i 's/GRUB_CMDLINE_LINUX.*$/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
 sudo sed -i "s/conf_force_conffold=YES/#conf_force_conffold=YES/g" /etc/ucf.conf
