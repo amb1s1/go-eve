bcd6b1a030a76dfa4f77a9782c91c31043514b82
amb1s1 <amb1s1@gmail.com>
2022-06-18 15:53:38 -0400

    Update install.sh
    Using sudo for the sed command, I ran into a problem where this command was not able to run because it was not run as root.

--- install.sh
+++ install.sh
@@ -12,5 +12,5 @@ wget -O - http://www.eve-ng.net/repo/install-eve.sh | bash -i
 sudo apt-get update
 sudo apt-get -y upgrade
 # Avoiding OS to rename eth0 interface to ens4
-sed -i 's/GRUB_CMDLINE_LINUX.*$/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
-sed -i "s/conf_force_conffold=YES/#conf_force_conffold=YES/g" /etc/ucf.conf
+sudo sed -i 's/GRUB_CMDLINE_LINUX.*$/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
+sudo sed -i "s/conf_force_conffold=YES/#conf_force_conffold=YES/g" /etc/ucf.conf
